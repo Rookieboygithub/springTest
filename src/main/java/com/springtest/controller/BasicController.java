bcec4b622151e5c5d912097f1402d3e3ff6ecb10
BHH <1326935715@qq.com>
2019-04-15 22:48:18 +0800

    springmvc基础

---
+++ src/main/java/com/springtest/controller/BasicController.java
@@ -0,0 +1,20 @@
+package com.springtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+	@GetMapping(value = "")
+	@ResponseBody
+	public ModelAndView index() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("index");
+		return model;
+	}
+}
